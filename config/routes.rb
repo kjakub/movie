17a69da40a18a0921bdd698ded195d6358c6de4d
Jakub Kuchar <jakub.kuchar@hotmail.com>
2015-09-16 13:09:47 +0200

    rspec re-generatted user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,7 +12,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
